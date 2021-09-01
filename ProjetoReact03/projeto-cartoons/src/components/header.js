80c4730d94386e6724f7456ffe4e6465151b4eb8
viniflorencio <viniflorenciomid@gmail.com>
2021-09-01 02:21:25 -0300

    atualizacao react

---
+++ ProjetoReact03/projeto-cartoons/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <nav>
+            <div className="logo"></div>
+            <ul>
+                <li>Home</li>
+                <li>Cartoons</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+    )
+}
