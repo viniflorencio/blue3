cc6d8c898e2cf798d001ef85d902ce79327bcc3a
viniflorencio <viniflorenciomid@gmail.com>
2021-09-01 02:22:02 -0300

    atualizacao react

---
+++ ProjetoReact03/projeto-cartoons/src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p> Todos Direitos reservados ViniFlorencio</p>
+            
+        </div>
+    )
+}
